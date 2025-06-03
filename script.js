dabb831c8670410dd8fb58863ac2b0bf21fee123
Tanbal <tanbal.derakhty@gmail.com>
2025-06-04 02:35:17 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,22 @@
-var game_list = document.querySelector(".game-list");
+var game_list = document.getElementById("game-list");
 const Games = [
-    {name: "Flappy Bird", url: "flappy-bird", image_url: ""},
-    {name: "Ping Pong", url: "ping-pong2", image_url: ""},
-    {name: "Bomb", url: "bomb2", image_url: "bomb/bomb.png"},
-    {name: "XO", url: "xo", image_url: ""},
-    {name: "2048", url: "2048(2)", image_url: ""},
+    {name: "minesweeper", url: "minesweeper/minesweeper", image_url: "bomb/bomb.png"},
+    {name: "XO", url: "xo/xo", image_url: ""},
     {name: "Sudoku", url: "sudoku", image_url: ""},
-    {name: "Chess", url: "chess3", image_url: ""},
-    {name: "Chess", url: "chess3", image_url: ""},
-    {name: "Chess", url: "chess3", image_url: ""},
-    {name: "Chess", url: "chess3", image_url: ""},
-    {name: "Chess", url: "chess3", image_url: ""},
-    {name: "Chess", url: "chess3", image_url: ""},
-    {name: "Chess", url: "chess3", image_url: ""},
-    {name: "Chess", url: "chess3", image_url: ""},
-    {name: "Chess", url: "chess3", image_url: ""},
-    {name: "Chess", url: "chess3", image_url: ""},
-    {name: "Chess", url: "chess3", image_url: ""}
+    {name: "2048", url: "2048/2048", image_url: ""}
 ];
 window.onload = function() {
     for (let a = 0 ; a < Games.length ; a++) {
         let [div, div_relative, name] = [document.createElement("div"),document.createElement("div"),document.createElement("div")];
         game_list.appendChild(div);
-        div_relative.style.backgroundImage = "url(../image/" + Games[a].image_url + ")";
+        // div_relative.style.backgroundImage = "url(../image/" + Games[a].image_url + ")";
         // div.onclick = function() {
-        //     window.location.href = "file:///C:/Users/Ghadir/Desktop/Code/html/" + Games[a].url + ".html";
+        //     window.location.href = "games/" Games[a].url + ".html";
         // }
         div.appendChild(div_relative);
         name.innerHTML = Games[a].name;
-        name.classList.add("game-name");
         name.onclick = function() {
-            window.location.href = "file:///C:/Users/Ghadir/Desktop/Code/html/" + Games[a].url + ".html";
+            window.location.href = "games/" + Games[a].url + ".html";
         }
         div_relative.appendChild(name);
     }
